97837f70bc732a9cbba4b258dba3fd04216d102e
itsyosefali <joeyxjoey123@gmail.com>
2024-01-11 12:46:32 +0200

    add button to send whatsapp messages

---
+++ frappe_whatsapp/api.py
@@ -0,0 +1,86 @@
+import frappe
+import json
+
+@frappe.whitelist()
+def send_whatsapp_message(customers,template,fields):
+    """Send WhatsApp message to a number"""
+    try:
+        fields = json.loads(fields)
+        customers = json.loads(customers)
+        template = frappe.db.get_value(
+                "WhatsApp Templates", template,
+                fieldname='*'
+            )
+
+        if template:
+            data = {
+                "messaging_product": "whatsapp",
+                "to": format_number(customers[0]),
+                "type": "template",
+                "template": {
+                    "name": template.name,
+                    "language": {
+                        "code": template.language_code
+                    },
+                    "components": []
+                }
+            }
+
+            if fields:
+                parameters = []
+                for field in fields:
+                    parameters.append({
+                        "type": "text",
+                        "text": field
+                    })
+
+                data['template']["components"] = [{
+                    "type": "body",
+                    "parameters": parameters
+                }]
+    except Exception as e:
+        frappe.error_log(frappe.get_traceback(), "WhatsApp Campaign Error")
+        frappe.throw("Error while sending WhatsApp Campaign Message")
+        print(e)
+    try:
+        notify(data)
+    except Exception as e:
+        frappe.throw(f"Failed to send message {str(e)}")
+def notify(data):
+    """Notify."""
+    settings = frappe.get_doc(
+        "WhatsApp Settings", "WhatsApp Settings",
+    )
+    token = settings.get_password("token")
+
+    headers = {
+        "authorization": f"Bearer {token}",
+        "content-type": "application/json"
+    }
+    try:
+        response = make_post_request(
+            f"{settings.url}/{settings.version}/{settings.phone_id}/messages",
+            headers=headers, data=json.dumps(data)
+        )
+        self.message_id = response['messages'][0]['id']
+
+    except Exception as e:
+        res = frappe.flags.integration_request.json()['error']
+        error_message = res.get('Error', res.get("message"))
+        frappe.get_doc({
+            "doctype": "WhatsApp Notification Log",
+            "template": "Text Message",
+            "meta_data": frappe.flags.integration_request.json()
+        }).insert(ignore_permissions=True)
+
+        frappe.throw(
+            msg=error_message,
+            title=res.get("error_user_title", "Error")
+        )
+        
+def format_number(number):
+    """Format number."""
+    if (number.startswith("+")):
+        number = number[1:len(number)]
+
+    return number
--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_campaign/whatsapp_campaign.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_campaign/whatsapp_campaign.js
@@ -2,9 +2,43 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('WhatsApp Campaign', {
-	// refresh: function(frm) {
+	refresh: function (frm) {
+		frm.add_custom_button(__("Send"), function () {
+			if (frm.doc.customers.length == 0) {
+				frappe.msgprint("Please Insert atleast One Customer");
+				highlight_field(frm, "customers");
+				return;
+			}
 
-	// }
+			if (!frm.doc.template) {
+				frappe.msgprint("Please select template");
+				highlight_field(frm, "template");
+				return;
+			}
+			const list_of_customers = frm.doc.customers.map((customer) => {
+				return customer.phone_number;
+			}
+			);
+			if (list_of_customers.length > 0) {
+				const fields = frm.doc.fields.map((customer) => {
+					return customer.field_name;
+				});
+				frappe.call({
+					method: "frappe_whatsapp.api.send_whatsapp_message",
+					args: {
+						customers: list_of_customers,
+						template: frm.doc.template,
+						fields: fields,
+					},
+					callback: function (r) {
+						if (r.message) {
+							frappe.msgprint("Message Sent Successfully");
+						}
+					},
+				});
+			}
+		});
+	},
 	template: function (frm) {
 		frm.trigger("load_template");
 	},
@@ -22,3 +56,31 @@ frappe.ui.form.on('WhatsApp Campaign', {
 		);
 	},
 });
+
+function highlight_field(frm, fieldname) {
+	let field = frm.get_field(fieldname);
+	if (!field) return;
+
+	let $el = field.$wrapper;
+
+	// set tab as active
+	if (field.tab && !field.tab.is_active()) {
+		field.tab.set_active();
+	}
+
+	if (field.section.is_collapsed()) {
+		field.section.collapse(false);
+	}
+
+	frappe.utils.scroll_to($el, true, 15);
+
+	let control_element = $el.closest(".frappe-control");
+	console.log($el);
+	// control_element.addClass("highlight");
+	control_element.css("background-color", "#FFB0B6"); // Lighter red color
+	setTimeout(() => {
+		// control_element.removeClass("highlight");
+		control_element.css("background-color", "");
+	}, 7000);
+	return true;
+}
