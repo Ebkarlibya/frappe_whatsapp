92059e650828dec004d5719a7833fb5d4fd8726e
itsyosefali <joeyxjoey123@gmail.com>
2024-01-14 13:42:02 +0200

    check if the phone number is a vlid phone number

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_campaign/whatsapp_campaign.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_campaign/whatsapp_campaign.js
@@ -3,41 +3,43 @@
 
 frappe.ui.form.on('WhatsApp Campaign', {
 	refresh: function (frm) {
-		frm.add_custom_button(__("Send"), function () {
-			if (frm.doc.customers.length == 0) {
-				frappe.msgprint("Please Insert atleast One Customer");
-				highlight_field(frm, "customers");
-				return;
-			}
+		if (!frm.doc.__islocal) {
+			frm.add_custom_button(__("Send Message"), function () {
+				if (frm.doc.customers.length == 0) {
+					frappe.msgprint("Please Insert atleast One Customer");
+					highlight_field(frm, "customers");
+					return;
+				}
 
-			if (!frm.doc.template) {
-				frappe.msgprint("Please select template");
-				highlight_field(frm, "template");
-				return;
-			}
-			const list_of_customers = frm.doc.customers.map((customer) => {
-				return customer.phone_number;
-			}
-			);
-			if (list_of_customers.length > 0) {
-				const fields = frm.doc.fields.map((customer) => {
-					return customer.field_name;
-				});
-				frappe.call({
-					method: "frappe_whatsapp.api.send_whatsapp_messages",
-					args: {
-						customers: list_of_customers,
-						template: frm.doc.template,
-						fields: fields,
-					},
-					callback: function (r) {
-						if (r.message) {
-							frappe.msgprint("Message Sent Successfully");
-						}
-					},
-				});
-			}
-		});
+				if (!frm.doc.template) {
+					frappe.msgprint("Please select template");
+					highlight_field(frm, "template");
+					return;
+				}
+				const list_of_customers = frm.doc.customers.map((customer) => {
+					return customer.phone_number;
+				}
+				);
+				if (list_of_customers.length > 0) {
+					const fields = frm.doc.fields.map((customer) => {
+						return customer.field_name;
+					});
+					frappe.call({
+						method: "frappe_whatsapp.api.send_whatsapp_messages",
+						args: {
+							customers: list_of_customers,
+							template: frm.doc.template,
+							fields: fields,
+						},
+						callback: function (r) {
+							if (r.message) {
+								frappe.msgprint("Message Sent Successfully");
+							}
+						},
+					});
+				}
+			}).addClass("btn-warning").css({ 'color': 'white', 'font-weight': 'bold', "background-color": "orange" });;
+		}
 	},
 	template: function (frm) {
 		frm.trigger("load_template");
--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_campaign/whatsapp_campaign.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_campaign/whatsapp_campaign.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2024, Shridhar Patil and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import validate_phone_number_with_country_code
 
 class WhatsAppCampaign(Document):
-	pass
+	def validate(self):
+		for phone in self.customers:
+			if phone.phone_number:
+				validate_phone_number_with_country_code(phone.phone_number,"phone_number")
